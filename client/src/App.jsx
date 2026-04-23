413d8c24d0ff474e2ef061db1223239ed1c4f1bb
Opy-22 <raduan.ahmed.opy@g.bracu.ac.bd>
2026-04-23 12:07:01 +0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import EmergencyReportPage from "./pages/EmergencyReportPage";
 import ReportSuccessPage from "./pages/ReportSuccessPage";
 import RescuerDashboardPage from "./pages/RescuerDashboardPage";
+import TrackingDashboardPage from "./pages/TrackingDashboardPage";
+import RescueInsightsPage from "./pages/RescueInsightsPage";
 
 function DashboardPage() {
   return <h1>Dashboard Page</h1>;
@@ -15,7 +17,8 @@ function App() {
         <Route path="/" element={<EmergencyReportPage />} />
         <Route path="/report-success" element={<ReportSuccessPage />} />
         <Route path="/rescuer-dashboard" element={<RescuerDashboardPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/tracking" element={<TrackingDashboardPage />} />
+        <Route path="/rescue-insights" element={<RescueInsightsPage />} />
       </Routes>
     </BrowserRouter>
   );
